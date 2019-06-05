5eb65c99323d43a1be8a05dd9aa02cbcab6a143b
Alexander Smith <AlexSSwift@gmail.com>
2019-06-05 15:07:20 -0700

    Timer is up and running. Need to fix the disappearing 10s place when the number gets into the single digits. Need to change the start button so it cycles threw functioning as a start, pause and resume button.

--- Alex Timer/ViewController.swift
+++ Alex Timer/ViewController.swift
@@ -8,13 +8,109 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+   
+    @IBOutlet weak var timerTimePicker: UIPickerView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeLabelView: UIView!
+  
+    var timer = Timer()
+    var time: String = "00:00:00"
+    var currentHour = 00
+    var currentMinute = 00
+    var currentSecond = 00
+    let hours = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+    let minutes = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
+    let seconds = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
+    
 
+    @IBAction func startPauseResumeButtonPressed(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
+        timeLabelView.isHidden = false
+        
+        
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
+     timer.invalidate()
+        timeLabelView.isHidden = true
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var numberOfRows: Int = 0
+        if component == 0 {
+            numberOfRows = hours.count
+        } else if component == 1 {
+            numberOfRows = minutes.count
+        } else if component == 2 {
+            numberOfRows = seconds.count
+        } else {
+            print("error: couldn't get number of rows")
+        }
+        return numberOfRows
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+      //  let time: [[Int]] = [hours, minutes, seconds]
+
+        var numbers: String = ""
+        if component == 0 {
+            numbers = "\(hours[row])"
+        } else if component == 1 {
+            numbers = "\(minutes[row])"
+        } else if component == 2 {
+            numbers = "\(seconds[row])"
+        } else {
+            print("error: couldn't get data")
+        }
+        return numbers
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currentHour = hours[pickerView.selectedRow(inComponent: 0)]
+        currentMinute = minutes[pickerView.selectedRow(inComponent: 1)]
+        currentSecond = seconds[pickerView.selectedRow(inComponent: 2)]
+        let time = "\(self.currentHour):\(self.currentMinute):\(self.currentSecond)"
+        timeLabel.text = time
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        timerTimePicker.delegate = self
+        timerTimePicker.dataSource = self
+        
     }
-
+    
+    @objc func action(){
+        //if seconds reaches 00 and minute is not 00, reduce minutes by 1 and set seconds to 59. if minutes reach 00 and hours is not 00, reduce hours by 1 and set minutes to 59.
+        currentSecond -= 1
+        
+        if currentSecond == 00 {
+            if currentMinute != 00 {
+                currentMinute -= 1
+                currentSecond = 59
+            }
+        }
+        if currentMinute == 00 {
+            if currentHour != 00 {
+                currentHour -= 1
+                currentMinute = 59
+            }
+        }
+        
+        let time = "\(self.currentHour):\(self.currentMinute):\(self.currentSecond)"
+        
+        
+        timeLabel.text = time
+    }
+    
+    
 
 }
 
